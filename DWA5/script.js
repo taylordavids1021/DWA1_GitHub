ddfe58049244c5609114d2c22ff4f548b15bc690
Taylor <taylordavids997@gmail.com>
2023-10-03 17:42:44 +0200

    Correctify comments

--- DWA5/script.js
+++ DWA5/script.js
@@ -11,14 +11,14 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = Math.floor(dividend / divider); //added Math.floor ()
+  result.innerText = Math.floor(dividend / divider); // Added Math.floor ()
 
   if (!dividend || !divider) {
     result.innerText = "Division not performed. Both values are required in inputs. Try again";
-} else if (dividend < 0 || divider < 0) {
+    } else if (dividend < 0 || divider < 0) {
     result.innerText = "Division not performed. Invalid number provided. Try again";
-} else if (isNaN(dividend) || isNaN(divider)) {
+    } else if (isNaN(dividend) || isNaN(divider)) {
     body.innerText = "Something critical went wrong. Please reload the page";
-   } else {
+    } else {
     result.innerText = Math.floor(dividend / divider);
-   }});
+}});
