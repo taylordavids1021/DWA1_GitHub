6bef6641909203f18d6c3aa83da0612bc80c382f
Taylor <taylordavids997@gmail.com>
2023-10-03 17:41:24 +0200

    add js file

---
+++ DWA5/script.js
@@ -0,0 +1,24 @@
+// scripts.js
+
+/**
+ * @param {number} dividend - number to be divided
+ * @param {number} divider - number to be divided by
+ */
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+const body = document.querySelector("[data-body]");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+  result.innerText = Math.floor(dividend / divider); //added Math.floor ()
+
+  if (!dividend || !divider) {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+} else if (dividend < 0 || divider < 0) {
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+} else if (isNaN(dividend) || isNaN(divider)) {
+    body.innerText = "Something critical went wrong. Please reload the page";
+   } else {
+    result.innerText = Math.floor(dividend / divider);
+   }});
